03418db06ea622be59b193392b16773052393d20
andresvilla1 <37849309+andresvilla1@users.noreply.github.com>
2019-02-15 14:35:52 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,21 +4,11 @@
 // "el" stands for the "element" that was just clicked
 function allOpaque(el){
 	el.classList.add("opaque");
-	document.getElementById("lakers-column").classList.add("opaque");
-	document.getElementById("red-column").classList.add("opaque");
-		document.getElementById("purple-column").classList.add("opaque");
-
 }
 
 // "el" stands for the "element" that was just clicked
 function removeColor(el){
-	document.getElementById("pats-column").classList.remove("pats-silver");
-	document.getElementById("rams-column").classList.remove("rams-blue");
 }
 
 function showIngram(el){
-	document.getElementById("laker").src = "image/ingramflextwo.png";
-	document.getElementById("lakertwo").src = "image/ingramflextwo.png";
-	document.getElementById("lakerthree").src = "image/ingramflextwo.png";
-
 }
